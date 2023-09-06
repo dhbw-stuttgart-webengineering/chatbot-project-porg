715a5cb4a1be7a4a8c9d4e129d252abaf15a3184
DrmedAllel <claudius.caspar.laur@gmail.com>
2023-09-06 14:58:07 +0200

    Merge branch 'main' into Claudius

--- Project Porg/frontend/hello.js
+++ Project Porg/frontend/hello.js
@@ -15,13 +15,13 @@ function sendMessage() {
 
 function receiveMessage(message) {
     if (message.trim() !== '') {
-        const userMessageContainer = document.createElement('div');
-        userMessageContainer.className = 'user-message';
-        const userMessage = document.createElement('div');
-        userMessage.className = 'message';
-        userMessage.textContent = userInput;
-        userMessageContainer.appendChild(userMessage);
-        document.querySelector('.user-message').appendChild(userMessageContainer);
-        document.getElementById('user-input').value = '';
+        const botMessageContainer = document.createElement('div');
+        botMessageContainer.className = message;
+        const botMessage = document.createElement('div');
+        botMessage.className = 'message';
+        botMessage.textContent = message;
+        botMessageContainer.appendChild(botMessage);
+        document.querySelector('.chat-container').appendChild(botMessageContainer);
+        message.value = '';
     }
 }
