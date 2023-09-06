b405a9fc532754917b05665478034f83fcfd2c32
risky <n11werthmann@gmail.com>
2023-09-06 16:05:13 +0200

    Button deaktivierung und farbliche Anpassungen, Scroll into View des Textfeldes, to-do: Scroll into view führt nach Eingabe dazu, dass der User das Logo nicht mehr sieht

--- Project Porg/frontend/hello.js
+++ Project Porg/frontend/hello.js
@@ -8,8 +8,11 @@ function sendMessage() {
         userMessage.className = 'message';
         userMessage.textContent = userInput;
         userMessageContainer.appendChild(userMessage);
-        document.querySelector('.user-message').appendChild(userMessageContainer);
+        document.querySelector('.chat-container').appendChild(userMessageContainer);
         document.getElementById('user-input').value = '';
+        userMessageContainer.scrollIntoView();
+        document.getElementById('sendMessage').disabled = true;
+        document.getElementById("sendMessage").style.backgroundColor= '#7d878d';
     }
 }
 
@@ -21,7 +24,10 @@ function receiveMessage(message) {
         botMessage.className = 'message';
         botMessage.textContent = message;
         botMessageContainer.appendChild(botMessage);
-        document.querySelector('.chat-container').appendChild(botMessageContainer);
+        document.querySelector('.chat-container').append(botMessageContainer);
         message.value = '';
+        botMessageContainer.scrollIntoView();
+        document.getElementById("sendMessage").disabled = false;
+        document.getElementById("sendMessage").style.backgroundColor= '#e2011b';
     }
 }
