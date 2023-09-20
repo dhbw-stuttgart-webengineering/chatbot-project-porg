26acc9b78a6625455d91cfee5eb281164a6a96f9
Kagan Demirer <demirerkagan0808@gmail.com>
2023-09-20 11:49:24 +0200

    fix

--- Project Porg/backend/app.py
+++ Project Porg/backend/app.py
@@ -24,7 +24,7 @@ def embedding(query):
 
 def search(query):
     embeds, _ = embedding(query)
-    res = pinecone_index.query(vector=embeds, top_k=3, include_metadata=True)
+    res = pinecone_index.query(vector=embeds, top_k=7, include_metadata=True)
     messages = [f"{i+1}. {message['metadata']['text']}. Quelle: {message['metadata']['link']}\n" for i, message in enumerate(res["matches"])]
     res = "".join(messages)
     print(res)
@@ -68,7 +68,7 @@ def checkForOldChatbots():
     delete = []
     for uuid in chatbots:
         print(uuid + ": " + str((datetime.datetime.now() - chatbots[uuid]["chatbot"]["lastUsed"]).total_seconds()))
-        if (datetime.datetime.now() - chatbots[uuid]["chatbot"]["lastUsed"]).total_seconds() > 300:
+        if (datetime.datetime.now() - chatbots[uuid]["chatbot"]["lastUsed"]).total_seconds() > 60:
             delete.append(uuid)
     for uuid in delete:
         print("Deleting chatbot uuid: " + uuid)
