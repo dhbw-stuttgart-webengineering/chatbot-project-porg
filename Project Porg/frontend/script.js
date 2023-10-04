5c43e1bade4fd918722adecc0b34bfb3202d80e7
DrmedAllel <claudius.caspar.laur@gmail.com>
2023-10-04 14:14:52 +0200

    Bug Fix beim Wechsel zwischen Mobile und Desktop

--- Project Porg/frontend/script.js
+++ Project Porg/frontend/script.js
@@ -655,5 +655,8 @@ function backToDesktop(){
         document.getElementsByClassName("chat")[0].style.display = "";
         document.getElementsByClassName("settings-container")[0].style.display = "";
         document.getElementsByClassName("link-container")[0].style.display = "";
+        document.getElementById("switchtochat").style.backgroundColor = "#5e6061";
+        document.getElementById("switchtosettings").style.backgroundColor = "#7d878d";
+        document.getElementById("switchtooverview").style.backgroundColor = "#7d878d";
     }
 }
