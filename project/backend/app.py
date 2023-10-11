2f32c0777f9bdc7fecc80298b47d56726ff3b068
Kagan Demirer <demirerkagan0808@gmail.com>
2023-10-11 12:18:00 +0200

    .

--- project/backend/app.py
+++ project/backend/app.py
@@ -147,4 +147,4 @@ def mail_api():
     return response
 
 if __name__ == "__main__":
-    app.run(port=int(os.environ.get("PORT", 8000)),host='0.0.0.0',debug=True)
+    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
